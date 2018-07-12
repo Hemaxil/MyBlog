5eacf06420de2b2e080dfbbe329761966c3c260b
hemaxi <hemaxilade@gmail.com>
2018-07-12 11:03:33 +0530

    rendering markdown into html using third party libs-pagedown and js lib marked

--- MyBlog/Blog/forms.py
+++ MyBlog/Blog/forms.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import User
 from django import forms
 from .models import *
-
+from pagedown.widgets import PagedownWidget
 class PostForm(forms.ModelForm):
+    publish=forms.DateField(widget=forms.SelectDateWidget)
+    content=forms.CharField(widget=PagedownWidget())
+
+
     class Meta:
         model=Post
         fields=['title','content','image','draft','publish',]
